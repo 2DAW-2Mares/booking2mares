caec158b626e482e0ceb56acc2fad8921fcbc318
Jose <takeskatevip@gmail.com>
2018-12-10 05:35:49 +0100

    En la mitad del examen

--- common/models/reserva.js
+++ common/models/reserva.js
@@ -1,5 +1,26 @@
 'use strict';
 
 module.exports = function(Reserva) {
+    Reserva.beforeRemote('create', function(ctx, reserva, next) {
+      ctx.args.data.usuarioId = ctx.req.accessToken.userId;
+      if (ctx.args.data.desde > ctx.args.data.hasta){
+        next(new Error('Comprueba las fechas'));
+      }else{
+        next();
+      }
+
+    });
+
+  /**
+   * Devuelve las reservas del dia actual
+   * @param {array} ctx context
+   * @param {Function(Error, array)} callback
+   */
+
+  Reserva.hoy = function(ctx, callback) {
+    var habReservadas = "hola";
+    console.log(ctx);
+    callback(null, habReservadas);
+  };
 
 };
