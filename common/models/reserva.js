3147dd37005b483242cdc0c6efc4ab680eadbdd5
Francisco <1542057@alu.murciaeduca.es>
2018-12-10 09:46:23 +0100

    Mitad del examen

--- common/models/reserva.js
+++ common/models/reserva.js
@@ -1,5 +1,8 @@
 'use strict';
 
 module.exports = function(Reserva) {
-
+    Reserva.beforeRemote('create', function (context, reserva, next) {
+        context.args.data.usuarioId = context.req.accessToken.userId;
+        next();
+    });
 };
