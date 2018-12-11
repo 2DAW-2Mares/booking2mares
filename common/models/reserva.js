9859d9274f7b8436205a599239c697723cf83873
maxi <you@example.com>
2018-12-11 11:13:41 +0100

    Entre la mitad del examen

--- common/models/reserva.js
+++ common/models/reserva.js
@@ -1,5 +1,11 @@
 'use strict';
 
 module.exports = function(Reserva) {
+    Reserva.beforeRemote('create', function(context, user, next) {
+      context.args.data.usuarioId = context.req.accessToken.userId;
 
-};
+     
+      
+      next();
+    });
+  };
