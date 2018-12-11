3cd72e52b438429a5f4fc3ce8ff075182a5b8624
Santiago <tconliguan@gmail.com>
2018-12-11 22:24:42 +0100

    Solucion del ejercicio examen

--- common/models/reserva.js
+++ common/models/reserva.js
@@ -2,4 +2,53 @@
 
 module.exports = function(Reserva) {
 
+  Reserva.beforeRemote('create', function (ctx, reserva, next) {
+    var userId = ctx.req.accessToken.userId;
+
+    ctx.args.data.usuarioId = userId;
+
+    var desde = ctx.args.data.desde;
+    var hasta = ctx.args.data.hasta;
+
+    var errorF = new Error("error de fecha");
+    
+    if (desde > hasta){
+      next(errorF);
+    } else{
+      next();
+    }
+
+
+  });
+
+  /**
+   * muestra las habitaciones reservadas de el día actual
+   * @param {Function(Error, array)} callback
+   */
+
+  Reserva.hoy = function(callback) {
+    var ocupadas = [];
+
+    var currentDate = new Date();
+    var fechaActual = currentDate.getDate() + currentDate.getHours();
+
+
+    Reserva.find(null, function (err, res) {
+      if (err) callback(err);
+
+      for (var i = 0; i < res.length; i++){
+        if (fechaActual >= res[i].desde && fechaActual <= res[i].hasta){
+          ocupadas.push(res[i]);
+        }
+      }
+
+
+      callback(null, ocupadas);
+
+    });
+
+
+  };
+
+
 };
