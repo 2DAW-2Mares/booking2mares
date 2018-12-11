3e64f64229e9aae863493fd9e10e38061ca8a49b
Antonio <atorralba96@gmail.com>
2018-12-11 11:14:19 +0100

    Mitad del examen

--- common/models/reserva.js
+++ common/models/reserva.js
@@ -1,5 +1,12 @@
 'use strict';
 
 module.exports = function(Reserva) {
-
+    Reserva.beforeRemote('create', function(context, user, next) {
+    
+    context.args.data.usuarioId = context.req.accessToken.userId;
+    if(context.args.data.desde>context.args.data.hasta){
+        context.args.data.desde="err, desde es mayor que hasta";
+    }
+    next();
+  });
 };
