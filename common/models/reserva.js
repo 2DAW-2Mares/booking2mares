ca7bf270076239bcd02cab0ec9fae5f4f1a27475
Emilio Carrelon <emiliocarrelon99@gmail.com>
2018-12-11 11:10:22 +0100

    En la mitad del examen

--- common/models/reserva.js
+++ common/models/reserva.js
@@ -1,5 +1,26 @@
 'use strict';
 
 module.exports = function(Reserva) {
+  Reserva.beforeRemote('create', function(context, user, next,err) {
+    context.args.data.usuarioId = context.req.accessToken.userId;
+    if (context.args.data.desde>context.args.data.hasta){
+        err=true;
+    }
+    if(err) throw "error en fechas";
+    next();
+  });
+
+  /**
+   *
+   * @param {date} Fecha
+   * @param {Function(Error, array)} callback
+   */
+
+  Reserva.hoy = function(Fecha, callback) {
+    var lista;
+    // TODO
+    callback(null, lista);
+  };
+
 
 };
