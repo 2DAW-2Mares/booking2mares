5abadf52a1eee4056b3a7632d10680f066cbaea7
maxi <you@example.com>
2018-12-11 12:22:25 +0100

    Solución del ejercicio de examen

--- common/models/reserva.js
+++ common/models/reserva.js
@@ -5,7 +5,28 @@ module.exports = function(Reserva) {
       context.args.data.usuarioId = context.req.accessToken.userId;
 
      
+      if(context.args.data.desde>context.args.data.hasta){
+          var mensaje="La fecha desde ha de ser inferior a la de hasta";
+        callback(null, mensaje);
+      }
       
       next();
     });
+
+Reserva.hoy = function(desde, hasta, callback) {
+   
+        Reserva.find( {where: { desde: desde, key2: {lt: 'val2'}{pension: true}, function(err, results) {
+        
+            return callback(err, results);
+        });
+        
+   
+
+    
+        callback(null, ocupadas);
+   
+    
+
+  };
+  
   };
