f67a1262db9abe78186943469e18f882071af9be
Facundo Jadur <facundojadur97@gmail.com>
2018-12-12 13:47:19 +0100

    mitad

--- common/models/reserva.js
+++ common/models/reserva.js
@@ -2,4 +2,17 @@
 
 module.exports = function(Reserva) {
 
+
+Reserva.beforeRemote('create', function(ctx, reserva, next){
+
+var err= new Error("Desde debe ser menor que Hasta");
+
+if(ctx.args.data.desde>ctx.args.data.hasta)
+next(err); 
+else
+next();
+
+});
+
+
 };
