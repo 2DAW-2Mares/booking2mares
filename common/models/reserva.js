dda5b90b251e05bcd1b9ad028a75908a2a6b41c3
Santiago <tconliguan@gmail.com>
2018-12-13 00:14:09 +0100

    examen2

--- common/models/reserva.js
+++ common/models/reserva.js
@@ -2,4 +2,18 @@
 
 module.exports = function(Reserva) {
 
+  Reserva.beforeRemote('create', function (ctx, user, next) {
+
+    var desde = ctx.args.data.desde;
+    var hasta = ctx.args.data.hasta;
+
+    if (desde > hasta){
+      next(new Error('Comprueba la fechas'));
+    }else {
+      next();
+    }
+
+
+  });
+
 };
