39f277995a56ffdf4e7af9e1334b53afc553352f
Antonio <atorralba96@gmail.com>
2018-12-12 13:20:03 +0100

    Solución del ejercicio de examen

--- common/models/hotel.js
+++ common/models/hotel.js
@@ -1,5 +1,29 @@
 'use strict';
 
 module.exports = function(Hotel) {
+    
+    Hotel.beforeRemote('create', function(context, user, next) {
+    
+    context.args.data.usuarioId = context.req.accessToken.userId;
+    for(i=0;i<3;i++){
+        context.args.data.numero= 10 +i;
+        context.args.data.categoria= "individual";
+        context.args.data.precio= 60;
+    }
+    next();
+  });
+  /**
+ * muestra habitaciones libres
+ * @param {date} desde "desdes" es la fecha que queremos entrar al hotel
+ * @param {date} hasta "hasta" es la fecha que queremos salir del hotel
+ * @param {Function(Error, string)} callback
+ */
 
+Hotel.primeraLibre = function(desde, hasta, callback) {
+  var lista;
+  // TODO
+  callback(null, lista);
+};
+
+  
 };
--- server/boot/automigrateDB.js
+++ server/boot/automigrateDB.js
@@ -2,7 +2,7 @@
 
 module.exports = function(app) {
   if (process.env.AUTOMIGRATE) {
-    app.dataSources.mysqlDS.automigrate(null, function(er) {
+    app.dataSources.mysqlDS.autoupdate(null, function(er) {
       if (er) throw er;
       console.log('Loopback tables created in ', app.dataSources.mysqlDS.adapter.name);
       app.loadFixtures()
